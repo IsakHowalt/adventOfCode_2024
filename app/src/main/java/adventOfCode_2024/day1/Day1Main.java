2fb77311143fc236469f6e299983e9860b507970
IsakHowalt <isakowe@gmail.om>
2024-12-01 21:18:04 +0100

    day1.2 done

--- app/src/main/java/adventOfCode_2024/day1/Day1Main.java
+++ app/src/main/java/adventOfCode_2024/day1/Day1Main.java
@@ -8,13 +8,15 @@ import java.util.ArrayList;
 public class Day1Main {
     private static ArrayList<Integer> leftArray;
     private static ArrayList<Integer> rightArray;
-    private static ArrayList<Integer> resultArray;
+    private static ArrayList<Integer> distanceArray;
+    private static ArrayList<Integer> similarityArray;
     public static void main(String[] args) throws FileNotFoundException {    
         Scanner scn = new Scanner(new File("app/src/main/java/adventOfCode_2024/day1/input/input.txt"));
             
         leftArray = new ArrayList<Integer>();
         rightArray = new ArrayList<Integer>();
-        resultArray = new ArrayList<Integer>();
+        distanceArray = new ArrayList<Integer>();
+        similarityArray = new ArrayList<Integer>();
 
         while(scn.hasNext()){
             String[] temp = scn.nextLine().split("   ");
@@ -31,19 +33,40 @@ public class Day1Main {
             int right = rightArray.get(i);
  
             if(left <= right){
-                resultArray.add(right-left);
+                distanceArray.add(right-left);
             }else{
-                resultArray.add(left-right);
+                distanceArray.add(left-right);
             }
       
         }
         int result = 0;
-        for (int i = 0; i < resultArray.size(); i++) {
-            result += resultArray.get(i);
+        for (int i = 0; i < distanceArray.size(); i++) {
+            result += distanceArray.get(i);
 
         }
+        System.out.println("Distance between lists: " + result);
+
+        for (int i = 0; i < leftArray.size(); i++) {
+            int left = leftArray.get(i);
+            int counter = 0;
+            for (int j = 0; j < rightArray.size(); j++) {
+                int right = rightArray.get(j);
+                if(left == right){
+                    counter++;
+
+                }
+
+            }
+            similarityArray.add(left*counter);
+        }
+
+        result = 0;
+        for (int i = 0; i < distanceArray.size(); i++) {
+            result += similarityArray.get(i);
+
+        }
+        System.out.println("Similarity Score: " + result);
         
-        System.out.println(result);
     }
 
 }
